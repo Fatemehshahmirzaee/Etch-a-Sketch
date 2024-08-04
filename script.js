d82d9e8a03425775659002974732e1dbf2bfa9e2
FatemehShahmirzaee <shahmirzaee.cgf@gmail.com>
2024-08-04 17:24:49 +0330

    active hover over mouse

--- script.js
+++ script.js
@@ -1,6 +1,5 @@
 const container = document.querySelector(".container");
-let div = document.createElement("div")
-
+let div2Array = [];
 
 for (let i=0; i<16; i++) {
     let div = document.createElement("div")  
@@ -8,8 +7,19 @@ for (let i=0; i<16; i++) {
 
         for (let j=0; j<16; j++) {
 
-            div.appendChild(document.createElement("div"))
-
-        }
-        
+            let div2 = document.createElement("div");
+            div.appendChild(div2);
+            div2Array.push(div2);
+        }       
 }
+
+for (let d of div2Array) {
+    d.addEventListener('click' , () => {
+
+        selectedArray.push(d)
+        d.style.backgroundColor = 'red';
+    })
+} 
+
+
+
